0a230a6f732bf5e3e6cf73f82da9244f471d6ce5
Atul Mishra <139470533+atulmishraaaa@users.noreply.github.com>
2024-02-13 19:54:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	if(num===0){
+		return 0;
+	}
+	else if(num===1){
+		return 1;
+	}
+	else{
+		let ans = [0,1];
+		for( let i = 2; i<=num.length; i++){
+			ans[i] = ans[i-1]+ ans[i-2];
+		}
+		return ans[num-1];
+		
+	}
+
+	
 }
 
 module.exports = fibonacci;
